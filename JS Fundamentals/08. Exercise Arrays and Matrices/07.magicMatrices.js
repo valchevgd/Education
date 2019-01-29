14bd34f0927d9bc480b222656c82a86b239fa5b1
Georgi Valchev <valchevgd@gmail.com>
2019-01-29 11:49:35 +0200

    JS Homework

---
+++ JS Fundamentals/08. Exercise Arrays and Matrices/07.magicMatrices.js
@@ -0,0 +1,31 @@
+function solve(matrix) {
+    let rowSum = 0;
+    for (let row = 0; row < matrix.length; row++){
+        let currentRowSum = 0;
+        for (let col = 0; col < matrix[row].length; col++){
+            currentRowSum += matrix[row][col];
+        }
+        if (row === 0){
+            rowSum = currentRowSum;
+        }else if (rowSum !== currentRowSum){
+            return false;
+        }
+    }
+    
+    for (let col = 0;col < matrix[0].length; col++){
+        let colsSum = 0;
+        for (let row = 0; row < matrix.length; row++){
+            colsSum += matrix[row][col];
+        }
+        if (colsSum !== rowSum){
+            return false
+        }
+
+    }
+
+    return true;
+}
+
+solve([[4, 5, 6],
+    [6, 5, 4],
+    [5, 5, 5]]);
